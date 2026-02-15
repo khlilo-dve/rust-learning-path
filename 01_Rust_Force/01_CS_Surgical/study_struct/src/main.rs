38904719be19fbd0af219212ce75f3f9b67b3521
YourName <you@example.com>
2026-02-15 13:34:58 +0800

    daily save

--- 01_Rust_Force/01_CS_Surgical/study_struct/src/main.rs
+++ 01_Rust_Force/01_CS_Surgical/study_struct/src/main.rs
@@ -4,12 +4,38 @@ struct Transaction {
     amount: u64,
 }
 
+impl Transaction {
+    fn new(from: String, to: String, amount: u64) -> Transaction {
+        Transaction {
+            from: from,
+            to: to,
+            amount: amount,
+        }
+    }
+    fn print_receipt(&self) {
+        println!(
+            "交易回执:{}->{}|数额:{}BTC",
+            self.from, self.to, self.amount
+        );
+    }
+    fn change_amount(&mut self, new_amount: u64) {
+        self.amount = new_amount;
+        println!("金额已修改为:{}", self.amount);
+    }
+}
+
+struct Blockchain{
+    ledger:Vec<Transaction>,
+}
+
+impl Blockchain{
+    fn new()->Blockchain{
+        Blockchain { 
+            ledger: Vec::new(),
+        }
+    }
+    fn
+}
 fn main() {
-    let tx1 = Transaction {
-        from: String::from("Alice"),
-        to: String::from("Bob"),
-        amount: 100,
-    };
-    println!("金额:{}", tx1.amount);
-    println!("收款人:{}", tx1.to);
+
 }
