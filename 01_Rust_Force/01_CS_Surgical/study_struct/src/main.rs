dac0c03eb35836dc8002ecac692a515d6ef5c9be
YourName <you@example.com>
2026-02-18 20:19:58 +0800

    完成简易区块链

--- 01_Rust_Force/01_CS_Surgical/study_struct/src/main.rs
+++ 01_Rust_Force/01_CS_Surgical/study_struct/src/main.rs
@@ -24,18 +24,34 @@ impl Transaction {
     }
 }
 
-struct Blockchain{
-    ledger:Vec<Transaction>,
+struct Blockchain {
+    ledger: Vec<Transaction>,
 }
 
-impl Blockchain{
-    fn new()->Blockchain{
-        Blockchain { 
-            ledger: Vec::new(),
-        }
+impl Blockchain {
+    fn new() -> Blockchain {
+        Blockchain { ledger: Vec::new() }
     }
-    fn
+    fn add_transaction(&mut self, tx: Transaction) {
+        self.ledger.push(tx);
+        println!("系统成功录入一笔交易");
+    }
+    fn print_chain(&self) {
+        println!("\n区块链账本数据");
+        for item in &self.ledger {
+            item.print_receipt();
+        }
+        println!("共计{}笔数据\n", self.ledger.len());
+    }
 }
 fn main() {
-
+    print!("区块链系统启动中");
+    let mut chain = Blockchain::new();
+    let tx1 = Transaction::new(String::from("Alice"), String::from("Bob"), 50);
+    let tx2 = Transaction::new(String::from("Bob"), String::from("Dave"), 20);
+    let tx3 = Transaction::new(String::from("Dave"), String::from("Eve"), 100);
+    chain.add_transaction(tx1);
+    chain.add_transaction(tx2);
+    chain.add_transaction(tx3);
+    chain.print_chain();
 }
