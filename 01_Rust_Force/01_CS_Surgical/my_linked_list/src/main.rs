7cb6b51dfa05d8bbde91690246aa269f51544855
khlilo-dve <linf75787@gmail.com>
2026-03-01 00:35:56 +0800

    feat(my_linked_list): implement safe head insertion and LIFO pop with Option take

--- 01_Rust_Force/01_CS_Surgical/my_linked_list/src/main.rs
+++ 01_Rust_Force/01_CS_Surgical/my_linked_list/src/main.rs
@@ -7,6 +7,46 @@ struct LinkedList{
     head:Option<Box<Node>>,
 }
 
+impl LinkedList{
+    fn new()->Self{
+        LinkedList{
+            head:None,
+        }
+    }
+    fn push(&mut self,value:u64){
+        let new_node=Box::new(Node{
+            value:value,
+            next:self.head.take()
+        });
+        self.head=Some(new_node);
+    }
+    fn pop(&mut self)->Option<u64>{
+        let old_head=self.head.take();
+        match old_head{
+            Some(node)=>{
+                self.head=node.next;
+                Some(node.value)
+            }
+            None=>None
+        }
+    }
+}
+
 fn main(){
-    println!("链表图纸编译通过");
+    let mut list=LinkedList::new();
+    list.push(100);
+    list.push(200);
+    println!("成功将100和200压入链表!");
+    match list.pop(){
+        Some(val)=>println!("弹出{}",val),
+        None=>println!("链表已经是空的了"),
+    }
+    match list.pop() {
+        Some(val) => println!("成功弹出了：{}", val),
+        None => println!("链表已经是空的了！"),
+    }
+    match list.pop() {
+        Some(val) => println!("成功弹出了：{}", val),
+        None => println!("链表已经是空的了！"),
+    }
 }
