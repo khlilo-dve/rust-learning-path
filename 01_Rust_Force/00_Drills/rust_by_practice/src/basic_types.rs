7dc2591dd2fa425eb812c3d0a88bf169a4795f56
khlilo-dve <linf75787@gmail.com>
2026-03-10 00:17:37 +0800

    fix: apply pragmatic tolerance for floating point expansion

--- 01_Rust_Force/00_Drills/rust_by_practice/src/basic_types.rs
+++ 01_Rust_Force/00_Drills/rust_by_practice/src/basic_types.rs
@@ -70,9 +70,9 @@ fn test_4_1_8() {
 }
 
 #[test]
-fn main() {
+fn test_4_1_9() {
     let mut sum = 0;
-    for i in -3..2 {
+    for i in -3..=2 {
         sum += i
     }
 
@@ -81,4 +81,42 @@ fn main() {
     for c in 'a'..='z' {
         println!("{}",c);
     }
+}
+
+#[test]
+// 填空
+fn test_4_1_10() {
+    use std::ops::{Range, RangeInclusive};
+    assert_eq!((1..5), Range{ start: 1, end: 5 });
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+}
+
+#[test]
+
+// 填空，并解决错误
+fn test_4_1_11() {
+    // 整数加法
+    assert!(1u32 + 2u32== 3);
+
+    // 整数减法
+    assert!(1i32 - 2 == -1);
+    assert!(1i8 - 2i8== -1);
+    
+    assert!(3 * 50 == 150);
+
+    assert!(9 / 3 ==3); // error ! 修改它让代码工作
+
+    assert!(24 % 5 == 4);
+    
+    // 逻辑与或非操作
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    // 位操作
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 }
